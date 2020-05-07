a8ebd6bd16d23bdbdec3394951a0f583fca6f708
Aimar Berrocal <aimarandony@gmail.com>
2020-05-07 09:29:29 -0500

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import gui.Ventas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ventas ventas = new Ventas();
+        ventas.setVisible(true);
+    }
+    
+}
